5ffa720886a157af38804010fcb87be5acbb9066
Dev-Odior <godspoweraino@gmail.com>
2024-07-10 13:56:15 +0100

    all articles

---
+++ index.js
@@ -0,0 +1,6 @@
+import { getStarted } from "./getStarted";
+import { settings } from "./settings";
+import { signUpArticles } from "./signUp";
+
+// export const articles = [...getStarted, ...settings, ...signUpArticles];
+export const articles = [];
